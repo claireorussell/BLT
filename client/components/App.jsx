cd3b69514e2f5cd4c22afbc706dd7796381ceaea
claire-russell <russell.claireolivia@gmail.com>
2020-03-13 12:15:34 +1300

    logo added

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -5,14 +5,18 @@ import initialQuestions from './initialQuestions'
 
 class App extends React.Component {
   render() {
-    return(
-  <Router>
-  <div className='app'>
-    <h1 className='mainTitle'> BLT </h1>
-    <Route exact path='/' component={initialQuestions}/>
-  </div>
-  </Router>
-    )}
+    return (
+      <Router>
+        <div className='app'>
+          <div className='titleWrapper'>
+            <img className='logo' src="https://images-na.ssl-images-amazon.com/images/I/41%2BK7DgSmkL._AC_SX355_.jpg" alt="" />
+            <h1 className='mainTitle'> BLT </h1>
+          </div>
+          <Route exact path='/' component={initialQuestions} />
+        </div>
+      </Router>
+    )
+  }
 }
 
 export default App
--- client/components/initialQuestions.jsx
+++ client/components/initialQuestions.jsx
@@ -9,25 +9,37 @@ class initialQuestions extends React.Component {
     render() {
         return (
             <div className='initialWrapper'>
-                  <h1 className='questionTitle'>What will guide your savings goals?</h1>
-            <div className='questionsWrapper'>
-                <div className='questionColumn1'>
-                    <button class="btn">save by $ amount</button>
+                <h1 className='questionTitle'>What will guide your savings goals?</h1>
+                <div className='questionsWrapper'>
+                    <div className='questionColumn1'>
+                        <button class="btn">save by $ amount</button>
+                    </div>
+                    <div className='questionColumn2'>
+                        <button class="btn">save by salary %</button>
+                    </div>
                 </div>
-                <div className='questionColumn2'>
-                    <button class="btn">save by salary %</button>
+                <h1 className='questionTitle'>Pick two things you like to spend money on as a treat!</h1>
+                <div className='questionsWrapper'>
+                    <div className='questionColumn1'>
+                        {/* change to input drop down */}
+                        <select id="option1" name="treat1">
+                            <option value="eating">Food and drink</option>
+                            <option value="entertainment">Entertainment</option>
+                            <option value="adventure">Adventure activies</option>
+                            <option value="materialistics">Materialistic stuff</option>
+                            <option value="vehicles">Vehicles</option>
+                        </select>
+                    </div>
+                    <div className='questionColumn2'>
+                        <select id="option2" name="treat2">
+                            <option value="eating">Food and drink</option>
+                            <option value="entertainment">Entertainment</option>
+                            <option value="adventure">Adventure activies</option>
+                            <option value="materialistics">Materialistic stuff</option>
+                            <option value="vehicles">Vehicles</option>
+                        </select>
+                    </div>
                 </div>
-            </div>
-            <h1 className='questionTitle'>Pick two things you like to spend money on as a treat!</h1>
-            <div className='questionsWrapper'>
-                <div className='questionColumn1'>
-                    {/* change to input drop down */}
-                    <button class="btn">save by $ amount</button>
-                </div>
-                <div className='questionColumn2'>
-                    <button class="btn">save by salary %</button>
-                </div>
-            </div>
             </div>
         )
     }

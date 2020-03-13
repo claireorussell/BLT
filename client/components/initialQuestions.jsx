36d2a5c811f8dcf669b8a2755606154b684a1fe7
claire-russell <russell.claireolivia@gmail.com>
2020-03-14 12:15:41 +1300

    initial page setUp complete

--- client/components/initialQuestions.jsx
+++ client/components/initialQuestions.jsx
@@ -9,17 +9,38 @@ class initialQuestions extends React.Component {
     render() {
         return (
             <div className='initialWrapper'>
-                <h1 className='questionTitle'>What will guide your savings goals?</h1>
-                <div className='questionsWrapper'>
-                    <div className='questionColumn1'>
-                        <button class="btn">save by $ amount</button>
-                    </div>
-                    <div className='questionColumn2'>
-                        <button class="btn">save by salary %</button>
-                    </div>
+
+                <h1 className='questionTitle'>What's your weekly wage?</h1>
+                <div className='questionWrapper'>
+                    $  <input
+                        class="input"
+                    // type='text'
+                    // name='name'
+                    // value={this.state.savingGoal}
+                    // onChange={this.handleChange}
+                    />
                 </div>
+
+
+                <h1 className='questionTitle'>What is your saving goal for the year?</h1>
+                <div className='questionWrapper'>
+                    $  <input
+                        class="input"
+                    // type='text'
+                    // name='name'
+                    // value={this.state.savingGoal}
+                    // onChange={this.handleChange}
+                    />
+                </div>
+
+                <h1 className='questionTitle'>Are you a home owner?</h1>
+                <div className='questionWrapper'>
+                    <button className="btn">yes</button>
+                    <button className="btn">no</button>
+                </div>
+
                 <h1 className='questionTitle'>Pick two things you like to spend money on as a treat!</h1>
-                <div className='questionsWrapper'>
+                <div className='questionWrapper'>
                     <div className='questionColumn1'>
                         {/* change to input drop down */}
                         <select id="option1" name="treat1">
@@ -40,6 +61,7 @@ class initialQuestions extends React.Component {
                         </select>
                     </div>
                 </div>
+                <button className='btn btnNext'>next</button>
             </div>
         )
     }

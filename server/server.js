9b5653e8b5fab2655b39b65f0661fad37c08966f
claire-russell <russell.claireolivia@gmail.com>
2020-03-19 16:19:43 +1300

    deploy

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 const path = require('path')
 const express = require('express')
 
-const budgetRoutes = require('./budget')
+// const budgetRoutes = require('./budget')
 const server = express()
 
 server.use(express.static(path.join(__dirname, '../public')))
 
-server.use('/api/v1/blt', budgetRoutes)
+// server.use('/api/v1/blt', budgetRoutes)
 
 module.exports = server
